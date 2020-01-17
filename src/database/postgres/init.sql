0ad326bbc79046471cc445e24532f96d8ac86306
pjozwiak <pjozwiak@future-processing.com>
2020-01-17 09:52:04 +0100

    added password column to table 'users'

--- src/database/postgres/init.sql
+++ src/database/postgres/init.sql
@@ -1,1 +1,1 @@
-CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), lastName varchar(255), firstName varchar(255));
+CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), lastName varchar(255), firstName varchar(255), password varchar(255));
