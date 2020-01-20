f72fd519f54bf04be4048a7aa329a574ce3419aa
pjozwiak <pjozwiak@future-processing.com>
2020-01-20 17:47:37 +0100

    implementing jwt

--- src/handlers/login.js
+++ src/handlers/login.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+const auth = require('../modules/user/authentication');
 
-module.exports = (req, h) => {
+module.exports = async (req, h) => {
   const user = {
     email: req.payload.email,
     firstname: req.payload.firstname,
-    lastname = req.payload.lastname,
-    password = req.payload.password
-  }
+    lastname: req.payload.lastname,
+    password: req.payload.password
+  };
 
-  const accessToken = jwt.sign(user, process.env.)
+  console.log('User authentication is: ');
+  console.log(auth.authenticateUser(user));
 
-}
-
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+  return accessToken;
+};
--- src/modules/user/authentication.js
+++ src/modules/user/authentication.js
@@ -1,5 +1,13 @@
 const db = require('../../database/postgres');
+const bcrypt = require('bcryptjs');
 
-exports.authenticateUser = user => {
+exports.authenticateUser = async user => {
   const email = user.email;
+  const existingUser = db.query(
+    "SELECT * FROM users WHERE email = '" + email + "'"
+  );
+
+  if (await bcrypt.compare(user.password, existingUser.password)) {
+    return true;
+  } else return false;
 };
