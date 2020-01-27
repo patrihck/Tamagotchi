f6a638136c54dff905caa02b49371229c06c814a
pjozwiak <pjozwiak@future-processing.com>
2020-01-27 09:03:10 +0100

    testing whether user was edited after the PATCH request

--- src/test/7_update-user.test.js
+++ src/test/7_update-user.test.js
@@ -1,6 +1,8 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
+const db = require('../database/postgres/db-context');
+const bcrypt = require('bcryptjs');
 
 chai.use(chaiHttp);
 chai.use(require('chai-json'));
@@ -8,19 +10,34 @@ chai.use(require('chai-json'));
 describe('Edit user', () => {
   describe('/PATCH', () => {
     it('Existing user should be updated with new data', done => {
+      const user = {
+        email: 'editeduser@email.com',
+        lastname: 'User',
+        firstname: 'Edited',
+        password: 'ihavejustbeenedited'
+      };
       chai
         .request('http://127.0.0.1:3001')
         .patch('/users/:1')
-        .send({
-          email: 'editeduser@email.com',
-          lastname: 'User',
-          firstname: 'Edited',
-          password: 'ihavejustbeenedited'
-        })
-        .end((err, res) => {
+        .send(user)
+        .end(async (err, res) => {
           expect(res).to.have.status(200);
-          done();
+          checkIfUserWasEdited(user).then(() => {
+            done();
+          });
         });
     });
   });
 });
+
+async function checkIfUserWasEdited(user) {
+  const queryResult = await db.query('SELECT * FROM users WHERE id = 1');
+  const userResult = queryResult.rows[0];
+
+  expect(user.email).to.equal(userResult.email);
+  expect(user.lastname).to.equal(userResult.lastname);
+  expect(user.firstname).to.equal(userResult.firstname);
+  expect(true).to.equal(
+    await bcrypt.compare(user.password, userResult.password)
+  );
+}
