25dbf527211ff4fa215843b67bfca087721a5784
pjozwiak <pjozwiak@future-processing.com>
2020-01-28 15:28:43 +0100

    corrected two first tests by using not-hardcoded url and methods placed in chai-helpers-methods.js

--- src/test/1_initial-get.test.js
+++ src/test/1_initial-get.test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
+const config = require('config');
 
 const init = require('../server.js');
 chai.use(chaiHttp);
@@ -8,15 +9,17 @@ chai.use(require('chai-json'));
 
 init.startServer();
 
+const url = `http://${config.appConfig.host}:${config.appConfig.port}`;
+
 describe('GET', () => {
   describe('/GET', () => {
     it('it should GET a simple json', done => {
       chai
-        .request('http://127.0.0.1:3001')
+        .request(url)
         .get('/')
         .end((err, res) => {
           expect(res).to.have.status(200);
-          //  expect(res).to.be.a.json;
+          expect(res).to.be.a.json;
           done();
           console.log(err);
         });
--- src/test/2_register-user.test.js
+++ src/test/2_register-user.test.js
@@ -1,27 +1,94 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
-
+const config = require('config');
+const chaiMethods = require('./chai-helper-methods');
 chai.use(chaiHttp);
 chai.use(require('chai-json'));
+
+const url = `http://${config.appConfig.host}:${config.appConfig.port}`;
+const endpoint = '/register';
 
 describe('Register user', () => {
   describe('/POST', () => {
     it('it should add an user to database and return status 200', done => {
-      chai
-        .request('http://127.0.0.1:3001')
-        .post('/register')
-        .send({
+      chaiMethods.makePostRequest(
+        url,
+        endpoint,
+        {
           email: 'email@email.com',
           lastname: 'Koń',
           firstname: 'Zdzisław',
-          password: 'OpOn@11!'
-        })
-        .end((err, res) => {
+          password: 'OpOn555555555@11!'
+        },
+        done,
+        (err, res) => {
           expect(res).to.have.status(200);
-          done();
-          console.log(err);
-        });
+        }
+      );
+    });
+  });
+});
+
+describe('Register user', () => {
+  describe('/POST', () => {
+    it('it should result with 406 because of no email', done => {
+      chaiMethods.makePostRequest(
+        url,
+        endpoint,
+        {
+          email: null,
+          lastname: 'Koń',
+          firstname: 'Zdzisław',
+          password: 'OpOn555555555@11!'
+        },
+        done,
+        (err, res) => {
+          expect(res).to.have.status(406);
+        }
+      );
+    });
+  });
+});
+
+describe('Register user', () => {
+  describe('/POST', () => {
+    it('it should result with 406 because of too short password', done => {
+      chaiMethods.makePostRequest(
+        url,
+        endpoint,
+        {
+          email: 'kameleon@email.com',
+          lastname: 'Góra',
+          firstname: 'Jan',
+          password: '123'
+        },
+        done,
+        (err, res) => {
+          expect(res).to.have.status(406);
+        }
+      );
+    });
+  });
+});
+
+describe('Register user', () => {
+  describe('/POST', () => {
+    it('it should result with 406 because of no password', done => {
+      chaiMethods.makePostRequest(
+        url,
+        endpoint,
+        {
+          email: 'kameleon@email.com',
+          lastname: 'Góra',
+          firstname: 'Jan',
+          password: undefined
+        },
+        done,
+        (err, res) => {
+          expect(res).to.have.status(406);
+        }
+      );
     });
   });
 });
