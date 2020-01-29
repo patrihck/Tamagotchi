127f9639245139f58909443721bd71670c00319f
pjozwiak <pjozwiak@future-processing.com>
2020-01-29 14:53:28 +0100

    upgraded login-success.test.js to avoid code doubling

--- src/test/3_login-success.test.js
+++ src/test/3_login-success.test.js
@@ -1,27 +1,31 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
-
+const chaiMethods = require('./chai-helper-methods');
+const config = require('config');
 chai.use(chaiHttp);
 chai.use(require('chai-json'));
 
+const url = `http://${config.appConfig.host}:${config.appConfig.port}`;
+const endpoint = '/login';
+
 describe('Login user', () => {
   describe('/POST', () => {
-    it('it should authenticate a user which is in DB', done => {
-      chai
-        .request('http://127.0.0.1:3001')
-        .post('/login')
-        .send({
+    it('registered user should be able to login', done => {
+      chaiMethods.makePostRequest(
+        url,
+        endpoint,
+        {
           email: 'email@email.com',
           lastname: 'Koń',
           firstname: 'Zdzisław',
-          password: 'OpOn@11!'
-        })
-        .end((err, res) => {
+          password: 'OpOn555555555@11!'
+        },
+        done,
+        (err, res) => {
           expect(res).to.have.status(200);
-          done();
-          console.log(err);
-        });
+        }
+      );
     });
   });
 });
