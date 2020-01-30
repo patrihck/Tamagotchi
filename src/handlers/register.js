d92c3669aecd440b14a2f11d05ea104f02714241
pjozwiak <pjozwiak@future-processing.com>
2020-01-30 09:46:32 +0100

    deleted unnecessary return

--- src/handlers/register.js
+++ src/handlers/register.js
@@ -8,21 +8,20 @@ module.exports = async (req, h) => {
     return h
       .response({ error: 'User with this email already exists' })
       .code(501);
-  } else {
-    const password = req.payload.password;
-    const salt = await bcrypt.genSaltSync();
-    const hashedPassword = await bcrypt.hash(password, salt);
-
-    const query =
-      'INSERT INTO users (firstName, password, lastName, email) values ($1, $2, $3, $4)';
-    const values = [
-      req.payload.firstName,
-      hashedPassword,
-      req.payload.lastName,
-      req.payload.email
-    ];
-
-    const response = await db.query(query, values);
-    return h.response(response).code(200);
   }
+  const password = req.payload.password;
+  const salt = await bcrypt.genSaltSync();
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const query =
+    'INSERT INTO users (firstName, password, lastName, email) values ($1, $2, $3, $4)';
+  const values = [
+    req.payload.firstName,
+    hashedPassword,
+    req.payload.lastName,
+    req.payload.email
+  ];
+
+  const response = await db.query(query, values);
+  return h.response(response).code(200);
 };
