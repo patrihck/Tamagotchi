b2f3806d41da3231d2d8152fbedd6a910259b4b1
pjozwiak <pjozwiak@future-processing.com>
2020-01-31 12:39:18 +0100

    deleted unnecessary try-catch from authenticateUser  function

--- src/services/user/authentication.js
+++ src/services/user/authentication.js
@@ -2,20 +2,11 @@ const db = require('../../database/postgres/db-context');
 const bcrypt = require('bcryptjs');
 
 exports.authenticateUser = async (email, password) => {
-  try {
-    const existingUser = (await db.findByEmail(email))[0];
+  const existingUser = (await db.findByEmail(email))[0];
 
-    if (!existingUser) {
-      return false;
-    }
-
-    if (await bcrypt.compare(password, existingUser.password)) {
-      return true;
-    } else {
-      return false;
-    }
-  } catch (err) {
-    console.log(err);
+  if (!existingUser) {
     return false;
   }
+
+  return await bcrypt.compare(password, existingUser.password);
 };
