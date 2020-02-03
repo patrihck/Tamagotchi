44d0aa90bcb8dfa4536cd2f316c9633ea14b5265
pjozwiak <pjozwiak@future-processing.com>
2020-02-03 12:25:49 +0100

    implemented saving time user was deleted at

--- src/database/postgres/init.sql
+++ src/database/postgres/init.sql
@@ -1,2 +1,2 @@
-CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), lastName varchar(255), firstName varchar(255), password varchar(255), IsDeleted boolean DEFAULT false,
+CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), lastName varchar(255), firstName varchar(255), password varchar(255), IsDeleted boolean DEFAULT false, deletedAt date,
 CONSTRAINT uc_email UNIQUE (email));
