eaf013499d5543f39c37c5d91c07e7502934ab82
pjozwiak <pjozwiak@future-processing.com>
2020-02-05 13:01:08 +0100

    added new table initialization to sql

--- src/database/postgres/init.sql
+++ src/database/postgres/init.sql
@@ -2,3 +2,8 @@ CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), l
 CONSTRAINT uc_email UNIQUE (email));
 
 CREATE TABLE IF NOT EXISTS petModifiers(id bigserial primary key, name varchar(128), property varchar(20), modifier integer, isDeleted boolean DEFAULT false, deletedAt date);
+
+CREATE TABLE IF NOT EXISTS petTypes(id bigserial primary key, name varchar(128), CONSTRAINT uc_name UNIQUE (name));
+
+
+CREATE TABLE IF NOT EXISTS petProperties(id bigserial primary key, petTypeId integer, name varchar(128), value integer, weight integer, valuePerTime integer);
