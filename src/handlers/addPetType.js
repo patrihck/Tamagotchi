7a4befd56b7ebad4061c1de54e5a419f0ec17c68
pjozwiak <pjozwiak@future-processing.com>
2020-02-05 14:25:36 +0100

    implemented all handler

--- src/handlers/addPetType.js
+++ src/handlers/addPetType.js
@@ -5,38 +5,38 @@ const Boom = require('@hapi/boom');
 
 module.exports = async (req, h) => {
   const properties = [];
-  let incomingProperties = req.payload.properties;
+  const incomingProperties = req.payload.properties;
 
   for (let i = 0; i < incomingProperties.length; i++) {
     properties.push(
       new PetProperty(
         null,
-        incomingProperties[i]['name'],
-        incomingProperties[i]['value'],
-        incomingProperties[i]['weight'],
-        incomingProperties[i]['valuePerTime']
+        incomingProperties[i].name,
+        incomingProperties[i].value,
+        incomingProperties[i].weight,
+        incomingProperties[i].valuePerTime
       )
     );
   }
+
+  console.log('PROPERTIES HERE ', properties);
   const petType = new PetType(req.payload.name, properties);
-  const petTypes = await db.getPetTypeByName(petType.name);
-  console.log(petTypes.length, 'PET TYPES LENGTH');
+  const petTypes = await db.getPetTypeByName(petType.name, req);
+
   if (petTypes.length === 0) {
     await db.addPetType(petType, req);
+    const petTypeId = (await db.getPetTypeByName(petType.name, req))[0].id;
 
-    const petTypeId = petTypes[0].id;
-    console.log(petTypeId, 'TUTAJ PETTYPEID');
-    // petType.properties.forEach(async property => {
-    //   property.petTypeId = petTypeId;
-    //   await db.addPetProperty(property, req);
-    // });
+    petType.properties.forEach(async property => {
+      property.petTypeId = petTypeId;
+      await db.addPetProperty(property, req);
+    });
   } else if (petTypes.length > 0) {
     return new Boom.conflict(
       'Pet type with that name already exists',
       petType.name
     );
   }
-  console.log(ids, 'HEREEEE', !ids);
 
   return h.response().code(200);
 };
