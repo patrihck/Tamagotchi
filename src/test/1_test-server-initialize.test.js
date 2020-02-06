51363ce261a6163057f8f4c31bc233e1c6440a46
pjozwiak <pjozwiak@future-processing.com>
2020-02-06 14:28:44 +0100

    added shared method which generates random id

--- src/test/1_test-server-initialize.test.js
+++ src/test/1_test-server-initialize.test.js
@@ -1,6 +1,11 @@
 const config = require('config');
 const init = require('../server.js');
 const url = `http://${config.appConfig.host}:${config.appConfig.port}`;
+const nanoid = require('nanoid');
+
+init.startServer();
 
 exports.url = url;
-init.startServer();
+exports.getRandomId = () => {
+  return nanoid();
+};
