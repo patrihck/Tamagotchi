1847cf6565cb9e8a5e946985a27cbadc7cd5156a
pjozwiak <pjozwiak@future-processing.com>
2020-02-11 09:07:38 +0100

    register user tests reworked

--- src/test/register-user.test.js
+++ src/test/register-user.test.js
@@ -9,90 +9,68 @@ chai.use(require('chai-json'));
 const endpoint = '/register';
 
 const user = {
-  email: 'email@email.com',
+  email: `email@email.com${testServer.getRandomId()}`,
   lastName: 'Koń',
   firstName: 'Zdzisław',
   password: 'OpOn555555555@11!'
 };
 
 describe('Register user', () => {
-  before(() => {
-    const id = nanoid(30);
-    user.email = id + user.email;
+  it('it should add an user to database and return status 200', async () => {
+    const res = await chai
+      .request(testServer.url)
+      .post(endpoint)
+      .send(user);
+    expect(res).to.have.status(200);
   });
 
-  it('it should add an user to database and return status 200', done => {
-    chaiMethods.makePostRequest(
-      testServer.url,
-      endpoint,
-      user,
-      done,
-      (err, res) => {
-        expect(res).to.have.status(200);
-      }
-    );
+  it('it should not be able to add already registered user', async () => {
+    const res = await chai
+      .request(testServer.url)
+      .post(endpoint)
+      .send(user);
+    expect(res).to.have.status(409);
   });
 
-  it('it should not be able to add already registered user', done => {
-    chaiMethods.makePostRequest(
-      testServer.url,
-      endpoint,
-      user,
-      done,
-      (err, res) => {
-        expect(res).to.have.status(409);
-      }
-    );
-  });
-
-  it('it should result with 406 because of no email', done => {
-    chaiMethods.makePostRequest(
-      testServer.url,
-      endpoint,
-      {
+  it('it should result with 406 because of no email', async () => {
+    const res = await chai
+      .request(testServer.url)
+      .post(endpoint)
+      .send({
         email: null,
         lastName: 'Koń',
         firstName: 'Zdzisław',
         password: 'OpOn555555555@11!'
-      },
-      done,
-      (err, res) => {
-        expect(res).to.have.status(400);
-      }
-    );
+      });
+
+    expect(res).to.have.status(400);
   });
 
-  it('it should result with 406 because of too short password', done => {
-    chaiMethods.makePostRequest(
-      testServer.url,
-      endpoint,
-      {
+  it('it should result with 406 because of too short password', async () => {
+    const res = await chai
+      .request(testServer.url)
+      .post(endpoint)
+      .send({
         email: 'kameleon@email.com',
         lastName: 'Góra',
         firstName: 'Jan',
         password: '123'
-      },
-      done,
-      (err, res) => {
-        expect(res).to.have.status(400);
-      }
-    );
+      });
+
+    expect(res).to.have.status(400);
   });
 
-  it('it should result with 406 because of no password', done => {
-    chaiMethods.makePostRequest(
-      testServer.url,
-      endpoint,
-      {
+  it('it should result with 406 because of no password', async () => {
+    const res = await chai
+      .request(testServer.url)
+      .post(endpoint)
+      .send({
         email: 'kameleon@email.com',
         lastName: 'Góra',
         firstName: 'Jan',
         password: undefined
-      },
-      done,
-      (err, res) => {
-        expect(res).to.have.status(400);
-      }
-    );
+      });
+
+    expect(res).to.have.status(400);
   });
 });
