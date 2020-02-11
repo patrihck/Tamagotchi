caa07c030b1f2ecfc9e0549e949faae2031d5fbc
pjozwiak <pjozwiak@future-processing.com>
2020-02-11 08:26:32 +0100

    initial get test reworked

--- src/test/initial-get.test.js
+++ src/test/initial-get.test.js
@@ -9,16 +9,10 @@ const url = `http://${config.appConfig.host}:${config.appConfig.port}`;
 
 describe('GET', () => {
   describe('/GET', () => {
-    it('it should GET a simple json', done => {
-      chai
-        .request(url)
-        .get('/')
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res).to.be.a.json;
-          done();
-          console.log(err);
-        });
+    it('it should GET a simple json', async () => {
+      const res = await chai.request(url).get('/');
+      expect(res).to.have.status(200);
+      expect(res).to.be.a.json;
     });
   });
 });
