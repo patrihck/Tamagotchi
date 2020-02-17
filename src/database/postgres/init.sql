864191bc1515547660377dee6cb2144b08529279
pjozwiak <pjozwiak@future-processing.com>
2020-02-17 13:01:54 +0100

    added UpdatedAt columns to petActions and users tables

--- src/database/postgres/init.sql
+++ src/database/postgres/init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), lastName varchar(255), firstName varchar(255), password varchar(255), deletedAt date, CONSTRAINT uc_email UNIQUE (email));
+CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), lastName varchar(255), firstName varchar(255), password varchar(255), updatedAt date, deletedAt date, CONSTRAINT uc_email UNIQUE (email));
 
 CREATE TABLE IF NOT EXISTS petModifiers(id bigserial primary key, name varchar(128), property varchar(20), modifier integer, petActionId integer, CONSTRAINT uc_modifier_name UNIQUE (name));
 
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS petTypes(id bigserial primary key, name varchar(128),
 
 CREATE TABLE IF NOT EXISTS petProperties(id bigserial primary key, petTypeId integer, name varchar(128), value integer, weight integer, valuePerTime integer);
 
-CREATE TABLE IF NOT EXISTS petActions(id bigserial primary key, petTypeId integer, name varchar(128), CONSTRAINT uc_action_name UNIQUE (name));
+CREATE TABLE IF NOT EXISTS petActions(id bigserial primary key, petTypeId integer, name varchar(128), updatedAt date, CONSTRAINT uc_action_name UNIQUE (name));
 
 CREATE TABLE IF NOT EXISTS pets(id bigserial primary key, petTypeId integer, userId integer, health integer default 100, name varchar(128), CONSTRAINT uc_pet_name UNIQUE (name)); 
