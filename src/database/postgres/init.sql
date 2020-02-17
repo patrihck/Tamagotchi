d84b71ccc88e71bd42f4408724924c44c628e1a1
pjozwiak <pjozwiak@future-processing.com>
2020-02-17 13:07:43 +0100

    added user roles table

--- src/database/postgres/init.sql
+++ src/database/postgres/init.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS users(id bigserial primary key, email varchar(255), lastName varchar(255), firstName varchar(255), password varchar(255), updatedAt date, deletedAt date, CONSTRAINT uc_email UNIQUE (email));
 
+CREATE TABLE IF NOT EXISTS userRoles(id bigserial primary key, name varchar(20), CONSTRAINT uc_rolename UNIQUE(name));
+
 CREATE TABLE IF NOT EXISTS petModifiers(id bigserial primary key, name varchar(128), property varchar(20), modifier integer, petActionId integer, CONSTRAINT uc_modifier_name UNIQUE (name));
 
 CREATE TABLE IF NOT EXISTS petTypes(id bigserial primary key, name varchar(128), CONSTRAINT uc_type_name UNIQUE (name));
